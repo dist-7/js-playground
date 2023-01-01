aa1d48289d9a8022caa4985ef30769ec0e80c7ef
dist-7 <118943280+dist-7@users.noreply.github.com>
2023-01-01 02:29:14 -0600

    Add files via upload

---
+++ assets/moon.72456e23.js
@@ -0,0 +1,10 @@
+const n=`<svg viewBox="0 0 24 24">
+  <path
+    d="M21 12.79A9 9 0 1 1 11.21 3A7 7 0 0 0 21 12.79z"
+    fill="none"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+  />
+</svg>`;export{n as default};
